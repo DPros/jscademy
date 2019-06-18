7601b5e0482448b160bd1099f43ef7ac78f43fe3
Ilya Sirosh <IlyaSirosh@users.noreply.github.com>
2019-06-18 23:51:32 +0300

    added console, console service, and text editor interaction

--- src/app/services/console.service.ts
+++ src/app/services/console.service.ts
@@ -2,27 +2,32 @@ import { Injectable } from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {TaskModel} from '../models';
 import {TaskService} from '../study/task.service';
-import {tryCatch} from 'rxjs/internal/util/tryCatch';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConsoleService {
-
+  private readonly empty_string = '!%$empty';
   private task = new BehaviorSubject(null);
   private clear = new BehaviorSubject(null);
   private code = new BehaviorSubject('');
-
+  private result = new BehaviorSubject('');
+  private oldCode = '';
 
   public readonly $clearTextEditor: Observable<any> = this.clear.asObservable();
   public readonly $code: Observable<string> = this.code.asObservable();
   public readonly $task: Observable<TaskModel> = this.task.asObservable();
+  public readonly $result: Observable<string> = this.result.asObservable();
 
-  constructor(private taskService: TaskService) { }
+  constructor(private taskService: TaskService) {
+
+  }
 
 
   public runScript(code: string): void {
-    // TODO run code from editor in console maybe call compute() and then just show
+    const result = this.compute(code);
+    if (this.empty_string !== result)
+      this.result.next(result);
   }
 
   public save(code: string, taskId: number = null, evaluate: boolean = null) {
@@ -32,22 +37,22 @@ export class ConsoleService {
       if(evaluate){
         const success = this.isCorrect(code, task.solution);
         this.taskService.saveTask(taskId, code, success);
-      }else{
+      } else {
         this.taskService.saveTaskCode(taskId, code);
+        this.code.next(this.oldCode);
       }
 
     }
   }
 
   public setTask(task: TaskModel): void {
+    this.oldCode = this.code.getValue();
     this.task.next(task);
   }
 
-  public setCode(code: string): void {
-    this.code.next(code);
-  }
 
   public clearEditor(): void {
+    this.oldCode = '';
     this.clear.next(null);
   }
 
@@ -59,13 +64,25 @@ export class ConsoleService {
 
   private compute(code: string): string {
     let result = null;
-
+    const consoleLogFunction = console.log;
+    console.log = (x) => {
+      if (x) {
+        this.result.next(x);
+      }
+    };
     try{
-      result = eval(`(${code})()`);
+      result = eval(code);
     } catch (e) {
+      console.error(e);
+    }
+    console.log = consoleLogFunction;
 
+    const arr = code.split('\n');
+    if (arr[arr.length - 1].match(/\s+/)) {
+      result = this.empty_string;
     }
 
     return result;
   }
+
 }
