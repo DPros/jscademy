62498f13151885500c9bcddebdd3493c12f17987
Ilya Sirosh <IlyaSirosh@users.noreply.github.com>
2019-06-18 20:05:25 +0300

    added console service to manage task. Need to be tested

--- src/app/services/console.service.ts
+++ src/app/services/console.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {TaskModel} from '../models';
+import {TaskService} from '../study/task.service';
+import {tryCatch} from 'rxjs/internal/util/tryCatch';
 
 @Injectable({
   providedIn: 'root'
@@ -16,15 +18,25 @@ export class ConsoleService {
   public readonly $code: Observable<string> = this.code.asObservable();
   public readonly $task: Observable<TaskModel> = this.task.asObservable();
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
 
   public runScript(code: string): void {
-    // TODO run code from editor in console
+    // TODO run code from editor in console maybe call compute() and then just show
   }
 
-  public save(code: string, taskId: number = null) {
-    // TODO save somehow the progress
+  public save(code: string, taskId: number = null, evaluate: boolean = null) {
+
+    const task = this.task.getValue();
+    if (task && taskId && task.taskId === taskId) {
+      if(evaluate){
+        const success = this.isCorrect(code, task.solution);
+        this.taskService.saveTask(taskId, code, success);
+      }else{
+        this.taskService.saveTaskCode(taskId, code);
+      }
+
+    }
   }
 
   public setTask(task: TaskModel): void {
@@ -37,5 +49,23 @@ export class ConsoleService {
 
   public clearEditor(): void {
     this.clear.next(null);
+  }
+
+  private isCorrect(code: string, solution: string): boolean {
+
+    return this.compute(code ) === solution;
+
+  }
+
+  private compute(code: string): string {
+    let result = null;
+
+    try{
+      result = eval(`(${code})()`);
+    } catch (e) {
+
+    }
+
+    return result;
   }
 }
