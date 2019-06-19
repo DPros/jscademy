485e5c1e07144ad0a7c9c136012c992548f0edb6
Ilya Sirosh <IlyaSirosh@users.noreply.github.com>
2019-06-20 01:11:54 +0300

    added console scrolling and input

--- src/app/study/console/console.component.ts
+++ src/app/study/console/console.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {ConsoleMessageType, ConsoleService} from '../../services/console.service';
 import {skip} from 'rxjs/internal/operators';
+
 
 @Component({
   selector: 'app-console',
@@ -10,13 +11,19 @@ import {skip} from 'rxjs/internal/operators';
 export class ConsoleComponent implements OnInit {
   Type = ConsoleMessageType;
   data = [];
-
+  code = '';
   constructor(private consoleService: ConsoleService) { }
 
   ngOnInit() {
     this.consoleService.$result.pipe(skip(1)).subscribe(result => {
+      this.code = '';
       this.data.unshift(...result);
     });
   }
 
+  run(event: KeyboardEvent): void {
+    if (event.key === 'Enter') {
+      this.consoleService.runScript(this.code);
+    }
+  }
 }
